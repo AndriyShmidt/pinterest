fb43b985e64ef63be9dadf2abaf4c84eff6881f4
AndriyShmidt <andrii.shmidt@snagasolutions.com>
2024-01-19 13:54:30 +0200

    f

--- pinterest_article.js
+++ pinterest_article.js
@@ -1,110 +1,117 @@
 import fetch from 'node-fetch';
 
-const apiKey = 'uqzmebqojezbivd2dmpakmj93j7gjm';
-const clientId = 'MTQ5NDI3NjpiNGRmNTc1N2ZmNzM2YTVjMmE4YjZkMWYzZGM0OGM3NzQwNWIzOGIy';
-const redirectUri = 'https://sportscore.io/';
-
+const code = '85039ce7fd834ef11235c3cf2768af4b5d5fd2af';
 let tokenData;
 let autopostData;
 
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
+//Get autopost is on or off
 async function fetchAutopost() {
-    try {
-        const response = await fetch(`https://sportscore.io/api/v1/autopost/settings/pinterest/`, {
-            method: 'GET',
-            headers: {
-                "accept": "application/json",
-                'X-API-Key': apiKey,
-            },
-        });
-
-        autopostData = await response.json();
-    } catch (error) {
-        console.error('Error fetching autopost data:', error);
-    }
+    fetch('https://sportscore.io/api/v1/autopost/settings/pinterest/', {
+        method: 'GET',
+        headers: {
+            "accept": "application/json",
+            'X-API-Key': 'uqzmebqojezbivd2dmpakmj93j7gjm',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        autopostData = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
+
+// Get access token 
 
 async function fetchOAuthToken(code) {
-    const url = 'https://api.pinterest.com/v5/oauth/token';
+  const url = 'https://api.pinterest.com/v5/oauth/token';
 
-    const headers = {
-        'Authorization': `Basic ${clientId}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
+  const headers = {
+      'Authorization': 'Basic MTQ5NDI3NjpiNGRmNTc1N2ZmNzM2YTVjMmE4YjZkMWYzZGM0OGM3NzQwNWIzOGIy',
+      'Content-Type': 'application/x-www-form-urlencoded'
+  };
 
-    const body = new URLSearchParams({
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': redirectUri
-    });
+  const body = new URLSearchParams({
+      'grant_type': 'authorization_code',
+      'code': code,
+      'redirect_uri': 'https://sportscore.io/'
+  });
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: body
-        });
-
-        tokenData = await response.json();
-    } catch (error) {
-        console.error('Error fetching OAuth token:', error);
-    }
+  try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+          body: body
+      });
+      tokenData = await response.json();
+  } catch (error) {
+      console.error('Error:', error);
+  }
 }
 
-async function refreshPinterestToken() {
-    const url = 'https://api.pinterest.com/v5/oauth/token';
+async function ff() {
+  try {
+      await fetchOAuthToken(code);
+      console.log('Token Data:', tokenData);
+  } catch (error) {
+      console.error('Error in fetching token:', error);
+  }
+};
 
-    const headers = {
-        'Authorization': `Basic ${clientId}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
+ff();
 
-    const body = new URLSearchParams({
-        'grant_type': 'refresh_token',
-        'refresh_token': tokenData.refresh_token,
-        'scope': 'boards:read',
-        'refresh_on': 'true'
-    });
+//Refresh token every 28 day
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: body
-        });
+async function refreshPinterestToken(tokenData) {
+  const url = 'https://api.pinterest.com/v5/oauth/token';
 
-        tokenData = await response.json();
-        console.log('Refreshed Token Data:', tokenData);
-    } catch (error) {
-        console.error('Error refreshing token:', error);
-    }
+  const headers = {
+      'Authorization': 'Basic MTQ5NDI3NjpiNGRmNTc1N2ZmNzM2YTVjMmE4YjZkMWYzZGM0OGM3NzQwNWIzOGIy',
+      'Content-Type': 'application/x-www-form-urlencoded'
+  };
+
+  const body = new URLSearchParams({
+      'grant_type': 'refresh_token',
+      'refresh_token': tokenData.refresh_token,
+      'scope': 'boards:read',
+      'refresh_on': 'true'
+  });
+
+  try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+          body: body
+      });
+      tokenData = await response.json();
+      console.log('Refreshed Token Data:', tokenData);
+  } catch (error) {
+      console.error('Error in refreshing token:', error);
+  }
 }
 
 function scheduleTokenRefresh() {
-    const days = 28;
-    const millisecondsPerDay = 24 * 60 * 60 * 1000;
-
-    setTimeout(async () => {
-        try {
-            await refreshPinterestToken();
-            scheduleTokenRefresh();
-        } catch (error) {
-            console.error('Failed to refresh token:', error);
-        }
-    }, days * millisecondsPerDay);
+  const days = 28;
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+  setTimeout(async () => {
+      try {
+          await refreshPinterestToken(tokenData);
+          scheduleTokenRefresh();
+      } catch (error) {
+          console.error('Failed to refresh token:', error);
+      }
+  }, days * millisecondsPerDay);
 }
 
 scheduleTokenRefresh();
 
+//create boards
 function createBoardsToPinterest() {
     const today = new Date();
-
     if (today.getDate() === 1) {
         const monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"];
+                            "July", "August", "September", "October", "November", "December"];
         const monthName = monthNames[today.getMonth()];
         const year = today.getFullYear();
 
@@ -116,7 +123,7 @@ function createBoardsToPinterest() {
         };
 
         const headers = {
-            'Authorization': `Bearer ${tokenData.access_token}`,
+            'Authorization': 'Bearer <Add your token here>',
             'Content-Type': 'application/json'
         };
 
@@ -125,31 +132,37 @@ function createBoardsToPinterest() {
             headers: headers,
             body: JSON.stringify(data)
         })
-            .then(response => response.json())
-            .then(data => console.log('Success:', data))
-            .catch((error) => console.error('Error creating board:', error));
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch((error) => console.error('Error:', error));
     }
 }
 
 setInterval(createBoardsToPinterest, 24 * 60 * 60 * 1000);
 
-async function getBoards() {
-    const url = 'https://api.pinterest.com/v5/boards';
+// get boards
 
-    const headers = {
-        'Authorization': `Bearer ${tokenData.access_token}`,
-        'Content-Type': 'application/json'
-    };
+async function getBoards(accessToken) {
+  const url = 'https://api.pinterest.com/v5/boards';
 
-    try {
-        const response = await fetch(url, { headers: headers });
-        const data = await response.json();
-        console.log('Boards:', data.items[0].id);
-        return data;
-    } catch (error) {
-        console.error('Error getting boards:', error);
-    }
+  const headers = {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+  };
+
+  try {
+      const response = await fetch(url, { headers: headers });
+      const data = await response.json();
+      console.log('start')
+      console.log('Boards:', data.items[0].id);
+      console.log('end')
+      return data;
+  } catch (error) {
+      console.error('Error in getting boards:', error);
+  }
 }
+
+// Post on printest
 
 async function createPinterestPin(token, boardId, title, description, imageUrl, link) {
     const url = 'https://api.pinterest.com/v5/pins';
@@ -180,53 +193,60 @@ async function createPinterestPin(token, boardId, title, description, imageUrl, 
         const responseData = await response.json();
         console.log('Success:', responseData);
     } catch (error) {
-        console.error('Error creating Pinterest pin:', error);
+        console.error('Error:', error);
     }
 }
 
+
+//Start post pinterest
 async function processItem(item, match) {
-    if (Number(item.state_display) && Number(item.state_display) < 2) {
-        const homeTeamName = item.home_team?.name || '';
-        const awayTeamName = item.away_team?.name || '';
-        const competitionName = match.competition?.name || '';
-        const venueName = item.venue?.name || '';
+  if (Number(item.state_display) && Number(item.state_display) < 2) {
+      const homeTeamName = item.home_team?.name || '';
+      const awayTeamName = item.away_team?.name || '';
+      const competitionName = match.competition?.name || '';
+      const venueName = item.venue?.name || '';
+    
+      const description = `🎌Match Started!🎌 \n\n💥⚽️💥 ${homeTeamName} vs ${awayTeamName} League: ${competitionName} 💥⚽️💥 \n\n #${homeTeamName.replace(/[^a-zA-Z]/g, "")} #${awayTeamName.replace(/[^a-zA-Z]/g, "")} #${competitionName.replace(/[^a-zA-Z]/g, "")} ${venueName ? '#' + venueName.replace(/[^a-zA-Z]/g, "") : ''}`
+      const board_id = await getBoards(tokenData.access_token);
 
-        const description = `🎌Match Started!🎌 \n\n💥⚽️💥 ${homeTeamName} vs ${awayTeamName} League: ${competitionName} 💥⚽️💥 \n\n #${homeTeamName.replace(/[^a-zA-Z]/g, "")} #${awayTeamName.replace(/[^a-zA-Z]/g, "")} #${competitionName.replace(/[^a-zA-Z]/g, "")} ${venueName ? '#' + venueName.replace(/[^a-zA-Z]/g, "") : ''}`;
-        const boardId = await getBoards();
-
-        await createPinterestPin(tokenData.access_token, boardId.items[0].id, `${homeTeamName} vs ${awayTeamName}`, description, item.social_picture, item.url);
-    }
+      await createPinterestPin(tokenData.access_token, board_id.items[0].id, `${homeTeamName} vs ${awayTeamName}`, description, item.social_picture, item.url);
+  }
 }
 
+// ===== MAKE POST ON PAGE =====
 async function getMatch(matches) {
-    for (const match of matches) {
-        for (const item of match.matches) {
-            await fetchAutopost();
-            console.log(autopostData);
-            if (autopostData.some(item => item.enabled === true)) {
+    await fetchAutopost();
+    console.log(autopostData);
+    
+    if (autopostData.some(item => item.enabled === true)) {
+        for (const match of matches) {
+            for (const item of match.matches) {
+          
                 await processItem(item, match);
             }
         }
     }
 }
 
-async function fetchData() {
-    try {
-        const response = await fetch(`https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0`, {
-            method: 'GET',
-            headers: {
-                "accept": "application/json",
-                'X-API-Key': apiKey,
-            },
-        });
-
-        const data = await response.json();
-        await getMatch(data.match_groups);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+// get data from Sport Score
+function fetchData() {
+    fetch('https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0', {
+        method: 'GET',
+        headers: {
+            "accept": "application/json",
+            'X-API-Key': 'uqzmebqojezbivd2dmpakmj93j7gjm',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        getMatch(data.match_groups);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
+// start every 1 minute
 setInterval(fetchData, 60000);
 
 fetchData();
